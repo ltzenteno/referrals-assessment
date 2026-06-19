a47ab104db466c7e8606bf780b64185acfc04bf5
Luis Zenteno <ltzenteno@gmail.com>
2026-06-19 11:21:04 -0600

    add referral model

--- backend/referrals/models.py
+++ backend/referrals/models.py
@@ -1,3 +1,38 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# TODO: Create your Referral model here
+
+class Referral(models.Model):
+
+    class Status(models.TextChoices):
+        INVITATION_SENT = "invitation_sent", "Invitation Sent"
+        APPLICATION_RECEIVED = "application_received", "Application Received"
+        JOINED = "joined", "Joined"
+        DECLINED = "declined", "Declined"
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    status = models.CharField(max_length=30, choices=Status.choices, default=Status.INVITATION_SENT)
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_sent_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs) -> None:
+        # even though the FE will send the email trimmed
+        # I am enforcing it here, also converting to lowercase before saving
+        self.email = self.email.strip().lower()
+        self.last_sent_at = timezone.now()
+        super().save(*args, **kwargs)
+
+    def rotate_token(self) -> None:
+        """
+        Rotates the token and updates last_sent_at to current time
+        """
+        self.token = uuid.uuid4()
+        self.last_sent_at = timezone.now()
+        self.save()
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} - {self.email}"
